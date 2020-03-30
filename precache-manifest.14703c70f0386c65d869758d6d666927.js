57d5d5ebf9f1a7e2ddcb55c2616de6a076e27887
s.ovchinnikov <s.ovchinnikov@unitedtraders.com>
2020-03-31 04:28:50 +1000

    init

---
+++ precache-manifest.14703c70f0386c65d869758d6d666927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db7b7249f95ebcbcd1d442a2461f754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1498fc86ebcc07c2da3a",
+    "url": "/static/css/main.e0f6907c.chunk.css"
+  },
+  {
+    "revision": "9154d5e3e5637d12ede5",
+    "url": "/static/js/2.f07b07bc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f07b07bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1498fc86ebcc07c2da3a",
+    "url": "/static/js/main.ee5c96a5.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
